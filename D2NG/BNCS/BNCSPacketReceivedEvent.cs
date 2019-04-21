5f2d0eb8a4775f7d495221ade567cdcaa746daaf
Dan K <daniel@kuwahara.ca>
2019-04-21 19:13:54 -0400

    Create Packet Received Event

--- D2NG/BNCS/BNCSPacketReceivedEvent.cs
+++ D2NG/BNCS/BNCSPacketReceivedEvent.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace D2NG
+namespace D2NG.BNCS
 {
-    abstract class BNCSEvent
+    class BNCSPacketReceivedEvent : BNCSEvent
     {
     }
 }
