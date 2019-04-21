fb3809c4995e998c2f337aa7e017a4c06c5cb2f4
Dan K <daniel@kuwahara.ca>
2019-04-21 13:39:45 -0400

    Check if BNCS is already connected

---
+++ D2NG/D2NG/BNCS/AlreadyConnectedException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace D2NG
+{
+    [Serializable]
+    internal class AlreadyConnectedException : Exception
+    {
+        public AlreadyConnectedException()
+        {
+        }
+
+        public AlreadyConnectedException(string message) : base(message)
+        {
+        }
+
+        public AlreadyConnectedException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected AlreadyConnectedException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
