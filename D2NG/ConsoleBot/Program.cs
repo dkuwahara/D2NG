98b9b2065b93128fe956e5b7b1d7dd59be4ddc02
Dan K <daniel@kuwahara.ca>
2019-04-21 13:16:53 -0400

    Add Console Bot project

---
+++ D2NG/ConsoleBot/Program.cs
@@ -0,0 +1,16 @@
+﻿using D2NG;
+using System;
+
+namespace ConsoleBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting bot");
+            D2NG.Client client = new Client();
+            Console.WriteLine("Connecting to Battle.Net");
+            client.ConnectToBattleNet("useast.battle.net");
+        }
+    }
+}
