fa6976e35a567db77d04e6678d04e52e50c04db0
Dan K <daniel@kuwahara.ca>
2019-04-21 13:01:22 -0400

    Add Connect() functionality to BNCS

---
+++ D2NG/D2NG/BNCSConnectException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace D2NG
+{
+    [Serializable]
+    internal class BNCSConnectException : Exception
+    {
+        public BNCSConnectException()
+        {
+        }
+
+        public BNCSConnectException(string message) : base(message)
+        {
+        }
+
+        public BNCSConnectException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected BNCSConnectException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
