5de80a45ba5ef193088481cf50fc19a921315a37
Dan K <daniel@kuwahara.ca>
2019-04-21 12:32:21 -0400

    Add defaults and constants for BNCS

--- D2NG/D2NG/BNCS.cs
+++ D2NG/D2NG/BNCS.cs
@@ -1,6 +1,46 @@
 ﻿namespace D2NG
 {
-    internal class BNCS
+    class BNCS
     {
+
+        /**
+         * Current version byte, update this on new patches
+         */
+        public static readonly byte VERSION = 0x0d;
+
+        /**
+         * Packet sent to authenticate version.
+         */
+        public static readonly byte[] AuthInfoPacket =
+        {
+            0xff, 0x50, 0x3a, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x36, 0x38, 0x58, 0x49, 0x50, 0x58, 0x32, 0x44,
+            VERSION, 0x00, 0x00, 0x00, 0x53, 0x55, 0x6e, 0x65,
+            0x55, 0xb4, 0x47, 0x40, 0x88, 0xff, 0xff, 0xff,
+            0x09, 0x04, 0x00, 0x00, 0x09, 0x04, 0x00, 0x00,
+            0x55, 0x53, 0x41, 0x00, 0x55, 0x6e, 0x69, 0x74,
+            0x65, 0x64, 0x20, 0x53, 0x74, 0x61, 0x74, 0x65,
+            0x73, 0x00
+        };
+
+        /**
+         * Default port used to connected to BNCS
+         */
+
+        public static readonly int DEFAULT_PORT = 6112;
+        /**
+         * Use this port to connect to BNCS
+         */
+        private readonly int _port;
+
+        public BNCS() 
+            : this(DEFAULT_PORT)
+        {
+        }
+
+        public BNCS(int port)
+        {
+            _port = port;
+        }
     }
 }
