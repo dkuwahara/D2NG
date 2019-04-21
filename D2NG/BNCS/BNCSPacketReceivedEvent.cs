5ba89423f1697cd19bc54518a00a3085586f0c0a
Dan K <daniel@kuwahara.ca>
2019-04-21 19:21:53 -0400

    Clean up BNCS Events

--- D2NG/BNCS/BNCSPacketReceivedEvent.cs
+++ D2NG/BNCS/BNCSPacketReceivedEvent.cs
@@ -1,10 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
-namespace D2NG.BNCS
+namespace D2NG
 {
     class BNCSPacketReceivedEvent : BNCSEvent
     {
+        private byte Type { get; set; }
+        private List<byte> Packet { get; set; }
+
+        public BNCSPacketReceivedEvent(List<byte> packet)
+        {
+            this.Packet = packet;
+            this.Type = packet[1];
+        }
     }
 }
