819783d93984c8c3c307b440fcd6527fbdfd729e
Daniel Kuwahara <daniel@kuwahara.ca>
2019-04-25 21:55:15 -0400

    Fix tests due to changes in CheckRevision

--- D2NGTests/BNCS/Login/CheckRevisionTests.cs
+++ D2NGTests/BNCS/Login/CheckRevisionTests.cs
@@ -9,38 +9,44 @@ namespace D2NG.BNCS.Login
         [Fact]
         public void Test()
         {
+            byte[] checksum = { 0x4f, 0x78, 0x39, 0x2b };
+
             var result = CheckRevisionV4.CheckRevision("0RFf+AAA");
-            var checksum = "4f78392b";
             Assert.Equal(0, result.Version);
-            Assert.Equal(1333279019, BitConverter.ToInt32(result.Checksum));
+            Assert.Equal(BitConverter.ToInt32(checksum), BitConverter.ToInt32(result.Checksum));
             Assert.Equal("qiNZCXbTGzoPvl76Y66lNfg=\0", Encoding.ASCII.GetString(result.Info));
-            Assert.Equal( checksum, BitConverter.ToInt32(result.Checksum).ToString("x"));
         }
 
         [Fact]
         public void Test2()
         {
+            byte[] checksum = { 0x70, 0x6d, 0x58, 0x4d };
+
             var result = CheckRevisionV4.CheckRevision("3ou3jQAA");
             Assert.Equal(0, result.Version);
-            Assert.Equal(1886214221, BitConverter.ToInt32(result.Checksum));
+            Assert.Equal(BitConverter.ToInt32(checksum), BitConverter.ToInt32(result.Checksum));
             Assert.Equal("g8AEAY1dgVaotHYLUzJgKJo=\0", Encoding.ASCII.GetString(result.Info));
         }
 
         [Fact]
         public void Test3()
         {
+            byte[] checksum = { 0x67, 0x67, 0x4c, 0x56 };
+
             var result = CheckRevisionV4.CheckRevision("ZYDAHQAA");
             Assert.Equal(0, result.Version);
-            Assert.Equal(1734822998, BitConverter.ToInt32(result.Checksum));
+            Assert.Equal(BitConverter.ToInt32(checksum), BitConverter.ToInt32(result.Checksum));
             Assert.Equal("+9kKei3b960lDtOxuQeURoI=\0", Encoding.ASCII.GetString(result.Info));
         }
 
         [Fact]
         public void Test4()
         {
+            byte[] checksum = { 0x65, 0x4c, 0x6f, 0x61 };
+
             var result = CheckRevisionV4.CheckRevision("Ry4VIgAA");
             Assert.Equal(0, result.Version);
-            Assert.Equal(1699508065, BitConverter.ToInt32(result.Checksum));
+            Assert.Equal(BitConverter.ToInt32(checksum), BitConverter.ToInt32(result.Checksum));
             Assert.Equal("Kyl+7a6yKVGWEj7rzhB+Q40=\0", Encoding.ASCII.GetString(result.Info));
         }
 
