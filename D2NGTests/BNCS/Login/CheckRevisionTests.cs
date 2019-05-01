cfad3f41160207287e0264986d9b4ab737605df8
Daniel Kuwahara <daniel@kuwahara.ca>
2019-05-01 19:56:09 -0400

    Fix CheckRevision tests

--- D2NGTests/BNCS/Login/CheckRevisionTests.cs
+++ D2NGTests/BNCS/Login/CheckRevisionTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Runtime.InteropServices;
 using System.Text;
 using Xunit;
 
@@ -9,7 +10,7 @@ namespace D2NG.BNCS.Login
         [Fact]
         public static void TestCheckRevision1()
         {
-            byte[] checksum = { 0x4f, 0x78, 0x39, 0x2b };
+            byte[] checksum = { 0x2b, 0x39, 0x78, 0x4f };
 
             var result = CheckRevisionV4.CheckRevision("0RFf+AAA");
             Assert.Equal(0, result.Version);
@@ -20,7 +21,7 @@ namespace D2NG.BNCS.Login
         [Fact]
         public static void TestCheckRevision2()
         {
-            byte[] checksum = { 0x70, 0x6d, 0x58, 0x4d };
+            byte[] checksum = { 0x4d, 0x58, 0x6d, 0x70 };
 
             var result = CheckRevisionV4.CheckRevision("3ou3jQAA");
             Assert.Equal(0, result.Version);
@@ -31,7 +32,7 @@ namespace D2NG.BNCS.Login
         [Fact]
         public static void TestCheckRevision3()
         {
-            byte[] checksum = { 0x67, 0x67, 0x4c, 0x56 };
+            byte[] checksum = { 0x56, 0x4c, 0x67, 0x67};
 
             var result = CheckRevisionV4.CheckRevision("ZYDAHQAA");
             Assert.Equal(0, result.Version);
@@ -42,7 +43,7 @@ namespace D2NG.BNCS.Login
         [Fact]
         public static void TestCheckRevision4()
         {
-            byte[] checksum = { 0x65, 0x4c, 0x6f, 0x61 };
+            byte[] checksum = { 0x61, 0x6f, 0x4c, 0x65 };
 
             var result = CheckRevisionV4.CheckRevision("Ry4VIgAA");
             Assert.Equal(0, result.Version);
