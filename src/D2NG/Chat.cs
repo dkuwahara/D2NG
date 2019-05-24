6a2a3b64032926b7bd7d858a1b057239d62e9965
Daniel Kuwahara <daniel@kuwahara.ca>
2019-05-24 18:19:53 -0400

    Add comments and validation for Chat

--- src/D2NG/Chat.cs
+++ src/D2NG/Chat.cs
@@ -1,7 +1,11 @@
 ﻿using D2NG.BNCS;
+using System.Linq;
 
 namespace D2NG
 {
+    /// <summary>
+    /// Interface for interacting with chat packets for BNCS
+    /// </summary>
     public class Chat
     {
         private BattleNetChatServer _bncs;
@@ -11,14 +15,29 @@ namespace D2NG
             this._bncs = bncs;
         }
 
+        /// <summary>
+        /// Join a channel
+        /// </summary>
+        /// <param name="channel">Name of the channel to join. Maximum 31 characters</param>
         public void JoinChannel(string channel)
         {
             _bncs.JoinChannel(channel);
         }
 
+        /// <summary>
+        /// Send a chat message or a command to Battle.net
+        /// </summary>
+        /// <param name="message">
+        /// Message to send to Battle.net, should only accept chars where the value is greater than 0x20. 
+        /// Maximum length is 223 characters. Any additional characters will be truncated
+        /// </param>
         public void Send(string message)
         {
-            _bncs.ChatCommand(message);
+            if (message.ToCharArray().Any(c => c < 0x20))
+            {
+                throw new ChatValidationException("Message contains invalid characters");
+            }
+            _bncs.ChatCommand(message.Trim());
         }
     }
 }
