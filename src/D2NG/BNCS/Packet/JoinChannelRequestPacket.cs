e434dc1cbc93d40a4c0f92cf9beadb7833bf758c
Daniel Kuwahara <daniel@kuwahara.ca>
2019-05-26 08:45:47 -0400

    Update string formatter for join channel flags

--- src/D2NG/BNCS/Packet/JoinChannelRequestPacket.cs
+++ src/D2NG/BNCS/Packet/JoinChannelRequestPacket.cs
@@ -18,7 +18,7 @@ namespace D2NG.BNCS.Packet
         {
             Log.Verbose($"JoinChannelRequestPacket\n" +
                 $"\tType: {Type}\n" +
-                $"\tFlags: 0x{flags:2X}\n" +
+                $"\tFlags: 0x{flags,2:2X}\n" +
                 $"\tChannel: {channel}\n");
         }
     }
