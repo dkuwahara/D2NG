6625a042c772e1e6714ecc35eadfedf46c3aed9b
Daniel Kuwahara <daniel@kuwahara.ca>
2019-06-08 20:05:18 -0400

    Add Distance calculation to Point

--- src/D2NG/D2GS/Point.cs
+++ src/D2NG/D2GS/Point.cs
@@ -1,4 +1,6 @@
-﻿namespace D2NG.D2GS
+﻿using System;
+
+namespace D2NG.D2GS
 {
     public class Point
     {
@@ -8,6 +10,9 @@
         public Point(ushort x, ushort y) => (X, Y) = (x, y);
 
         public void Deconstruct(out ushort x, out ushort y) => (x, y) = (X, Y);
+
+        public double Distance(Point other) 
+            => Math.Sqrt(Math.Pow(X - other.X, 2.0) + Math.Pow(Y - other.Y, 2.0));
 
         public override string ToString() => $"({X}, {Y})";
     }
